b782404d49204db4b51c93619208598e7f0bc056
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-13 14:31:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-sp2tkh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-sp2tkh)
---
+++ script.js
@@ -0,0 +1,26 @@
+// function sayHi(name){
+//   console.log(`Hey ${name}!`);
+// }
+// const name = "Alex";
+// sayHi(name);
+
+
+
+
+
+// function sum(num1, num2){
+//   const res = num1 + num2;
+//   return res;
+// }
+
+// const name = sum(2, 2);
+
+// console.log(name);
+
+
+function getArea(width, height){
+  const area = width * height;
+  return area;
+}
+
+getArea(10, 20);
