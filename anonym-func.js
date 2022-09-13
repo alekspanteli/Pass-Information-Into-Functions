5c89b36776c2654a810b82e8911e1c903fa298e5
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-13 16:51:31 +0300

    1 file modified

--- anonym-func.js
+++ anonym-func.js
@@ -4,10 +4,20 @@
 
 // console.log(sayHi("Aleasdx"));
 
+
+// Function is executed as JS moves them always to top is called hoisting
+// console.log(getResult(10));
+
+// function getResult(result){
+//   let test = result + 2;
+//   return test;
+// };
+
+
+// Function expression is not moved to top
 console.log(getResult(10));
 
-function getResult(result){
+const getResult = function(result){
   let test = result + 2;
   return test;
 };
-
